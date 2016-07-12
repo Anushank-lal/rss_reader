8d995370296f5173df7c438ae686a1f4c6fff9d8
anushank-lal <anushanklal@gmail.com>
2016-07-12 18:23:41 +0530

    Remove unused action

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,10 +1,6 @@
 class EntriesController < ApplicationController
   before_action :set_feed, only: :index
 
-  def index
-    @entries = @feed.entries.order('published desc')
-  end
-
   def show
     @entry = Entry.find(params[:id])
   end
